756a044baade66018f8319d2f8ad1032dc153a36
alex-mac <meet.alexmac@gmail.com>
2017-01-05 20:19:59 -0800

    implement smooth scroll

--- js/main.js
+++ js/main.js
@@ -23,4 +23,5 @@ jQuery(document).ready(function($){
 			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
 		});
 	}
+
 });
